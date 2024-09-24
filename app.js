c41cc6e69cff7a4e643d7e342e70a61696cf086a
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-09-24 10:12:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Created by Tivotal */
+
+let typed = new Typed(".type-text", {
+  strings: ["Web Developer", "Content Creator", "Data Analyst"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
